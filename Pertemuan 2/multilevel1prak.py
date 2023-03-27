9f840c022d3e826624dd5bad8219a44ac266583b
NandaLestari13 <127703685+NandaLestari13@users.noreply.github.com>
2023-03-27 22:00:36 +0700

    Add files via upload

---
+++ Pertemuan 2/multilevel1prak.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, species):
+        self.species = species
+    def eat(self):
+        print("The animal is eating.")
+class Pet(Animal):
+    def __init__(self, name, species):
+        super().__init__(species)
+        self.name = name
+    def play(self):
+        print("The pet is playing.")
+class cat(Pet):
+    def __init__(self, name, breed):
+        super().__init__(name, "Canine")
+        self.breed = breed
+    def bark(self):
+        print("Meow! MeoW!")
+
+my_cat = cat("Anu", "Golden Retriever")
+print("Species:", my_cat.species)
+print("Name:", my_cat.name)
+my_cat.eat()
+my_cat.play()
+my_cat.bark()
